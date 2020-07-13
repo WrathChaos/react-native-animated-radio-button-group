4e454bfcc6492e32b1fe9a7b4c2aad9c8b266797
FreakyCoder <kurayogun@gmail.com>
2020-07-13 15:07:34 +0300

    Horizontal feature is here and new example with RN 0.63

--- example/App.js
+++ example/App.js
@@ -1,21 +1,28 @@
 import React, { useState } from "react";
 import { View, Text, StatusBar, SafeAreaView } from "react-native";
-import RadioButtonGroup from "react-native-animated-radio-button-group";
+// import RadioButtonGroup from "react-native-animated-radio-button-group";
+import RadioButtonGroup from "./lib/src/RadioButtonGroup";
 
 const App = () => {
   const [item, setItem] = useState({
     id: 2,
     text: "Gaming",
     color: "#F6800F",
-    size: 25
+    size: 25,
   });
 
   const data = [
-    { id: 0, text: "Cinema", color: "#FF0004", size: 25 },
-    { id: 1, text: "Theatre", color: "#FB4009", size: 25 },
-    { id: 2, text: "Gaming", color: "#F6800F", size: 25 },
-    { id: 3, text: "Travelling", color: "#F4A012", size: 25 },
-    { id: 4, text: "Sport", color: "#F0E017", size: 25 }
+    { id: 0, text: "Cinema", color: "#FF0004", size: 25, horizontal: false },
+    { id: 1, text: "Theatre", color: "#FB4009", size: 25, horizontal: false },
+    { id: 2, text: "Gaming", color: "#F6800F", size: 25, horizontal: false },
+    {
+      id: 3,
+      text: "Travelling",
+      color: "#F4A012",
+      size: 25,
+      horizontal: false,
+    },
+    { id: 4, text: "Sport", color: "#F0E017", size: 25, horizontal: false },
   ];
   return (
     <View>
@@ -24,13 +31,14 @@ const App = () => {
         style={{
           marginTop: "30%",
           alignItems: "center",
-          justifyContent: "center"
+          justifyContent: "center",
         }}
       >
         <RadioButtonGroup
           initial={2}
           data={data}
-          onChange={item => setItem(item)}
+          horizontal={false}
+          onChange={(item) => setItem(item)}
         />
         <View style={{ margin: 16, top: "50%" }}>
           <Text>{`ID: ${item.id}`}</Text>
