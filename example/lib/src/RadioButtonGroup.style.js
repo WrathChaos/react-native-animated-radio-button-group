3486869e059ade62bb28cfcb17713f641bde9e5d
FreakyCoder <kurayogun@gmail.com>
2020-07-20 15:24:29 +0300

    Styling Android crash fix

--- example/lib/src/RadioButtonGroup.style.js
+++ example/lib/src/RadioButtonGroup.style.js
@@ -1,10 +1,10 @@
 export const _eachContainer = (horizontal) => ({
-  marginTop: !horizontal && 8,
-  marginLeft: horizontal && 12,
+  marginTop: horizontal ? 0 : 8,
+  marginLeft: horizontal ? 12 : 0,
 });
 
 export const _container = (horizontal) => ({
-  flexDirection: horizontal && "row",
+  flexDirection: horizontal ? "row" : "column",
 });
 
 export default {};
